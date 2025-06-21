e465673b3d5789b871c6450baa72bace82688533
applecider <arizah.applecider+github@gmail.com>
2025-06-21 10:43:51 -0600

    feat :sparkles: A0-40-ARIZPE | make service for auth token

--- src/app/controllers/auth/auth.controller.ts
+++
@@ -1,8 +0,0 @@
-
-export const saveTokenToLocalStorage = (token : string ) : void => {
-  localStorage.setItem('JWT', token);
-}
-
-export const getTokenFromLocalStorage = (): string | null => {
-  return localStorage.getItem('JWT');
-}
