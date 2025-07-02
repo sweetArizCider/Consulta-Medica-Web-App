040b21c122fcd5c567c5a9fe0ce43e4b3f49aa5b
RaulHMV <23170067@uttcampus.edu.mx>
2025-07-01 20:29:09 -0600

    Feat 💄 A0-46-VICTOR | loader feature

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginView } from '../views/login/login.view';
+import { RegisterView } from '../views/register/register.view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginView
+  },
+  {
+    path: 'register',
+    component: RegisterView
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  }
+];
